558b1a2f19acda00d5446cab626453f46bd36634
Richard Carvalho <richardcarvalhooo@gmail.com>
2024-07-04 14:24:39 +0000

    improvement: updating ccache script

--- setup/ccache.sh
+++ setup/ccache.sh
@@ -9,8 +9,10 @@ ccache -M 75G
 sudo mkdir -p /mnt/ccache
 mkdir -p ~/.cache/ccache
 sudo mount --bind "$HOME/.cache/ccache" /mnt/ccache
-echo "export USE_CCACHE=1" >> "$HOME/.zshrc"
-echo "export CCACHE_EXEC=/usr/bin/ccache" >> "$HOME/.zshrc"
-echo "export CCACHE_DIR=/mnt/ccache" >> "$HOME/.zshrc"
+echo "" >> "$HOME/.bashrc"
+echo "export USE_CCACHE=1" >> "$HOME/.bashrc"
+echo "export CCACHE_EXEC=/usr/bin/ccache" >> "$HOME/.bashrc"
+echo "export CCACHE_DIR=/mnt/ccache" >> "$HOME/.bashrc"
 echo "$HOME/.cache/ccache /mnt/ccache none defaults,bind,users,noauto 0 0" | sudo tee -a /etc/fstab
+echo "" >> "$HOME/.profile"
 echo "mount /mnt/ccache" >> "$HOME/.profile"
